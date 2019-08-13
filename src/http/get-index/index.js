db276dc737a8ed0b9d02dfc982e44df443217ccd
Ryan Betts <ryanbetts@gmail.com>
2019-08-12 19:35:37 -0700

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -9,7 +9,7 @@ exports.handler = async function Index () {
      */
     fullname: 'Your Name', // ←  Start by adding your name!
     title: 'My personal site!',
-    occupation: 'Artist & Photographer',
+    occupation: 'Sandwich Artist',
     location: 'West Glacier, MT',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
 
